579f86953e0c7d8661d516b2430fe4df9e1676bf
pbook <myhamidi@hotmail.de>
2019-06-01 17:36:55 +0200

    Var rename

--- testSim.py
+++ testSim.py
@@ -1,7 +1,7 @@
 import CCSim
-speed = 80
+StartSpeed = 80
 
-testSim = CCSim.clsCCSim(speed,speed/3)
+testSim = CCSim.clsCCSim(StartSpeed,StartSpeed/3)
 
 testSim.setAccBehaviour([(0.1,1)],[(0.1,-1)])
 
