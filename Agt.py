4769dc79aabe5e4f3bd291b419ec2916bbfac0ad
pbook <myhamidi@hotmail.de>
2019-06-06 07:00:25 +0200

    Q-Learning Agent

--- Agt.py
+++ Agt.py
@@ -19,31 +19,39 @@ class clsAgent:
         self.TransitionMatrix = []  #Typ: List of Lists [[],[],...]
         self.actions = actionlist
         self.LastAction = ""
+        self.LastActionInt = 0
+        self.Q = []
+        #Parameters:
+        self.alpha = .2
+        self.gamma = 1
 
     def PerceiveState(self,strState,reward):
         tr.call("clsAgent.PerceiveState")
         idx = self.pvRetIndex(strState,reward)
         if len(self.SequenceRewards) == 0:r=0
         else: _,r,_ = self.SequenceRewards[-1]
-        self.SequenceRewards.append((idx,r+reward,self.LastAction))
+        self.SequenceRewards.append((idx,round(r+reward,1),self.LastActionInt))
         self.pvExtendTransitionMatrix()
+        self.pvUpdateQ(self.alpha,self.gamma)
 
     def RetNextAction(self):
         tr.call("clsAgent.RetNextAction")
+        #Random
         self.LastAction = random.choice(self.actions)
+        self.LastActionInt =self.actions.index(self.LastAction)
+        # #Greedy
+        # s,_,_ = self.SequenceRewards[-1]        
+        # self.LastActionInt = self.Q[s].index(max(self.Q[s]))
+        # self.LastAction = self.actions[self.LastActionInt]
+        #Return
         return self.LastAction
 
     def IntiSequences(self):
         self.SequenceRewards =[]
-
-    def StateIsTerminal(self):
-        tr.call("clsAgent.StateIsTerminal")
-        if len(self.SequenceRewards) == 0:
-            return False
-        idx,_,_ = self.SequenceRewards[-1]
-        if self.RewStates[idx].state[-8:] == "terminal":
-            return True
-        return False
+    
+    def InitQ(self,alpha, gamma):
+        self.alpha = alpha
+        self.gamma = gamma
     
     def DoBellman(self,iterations):
         tmpVF = [self.RewStates[i].value for i in range(len(self.RewStates))]
@@ -66,19 +74,24 @@ class clsAgent:
             self.RewStates[idx].value += (rsum-rx-self.RewStates[idx].value)/self.RewStates[idx].visited
 
 
+
 #Private:
     def pvAppendNewState(self,RewardState):
         self.RewStates.append(RewardState)
         self.TransitionMatrix.append([])
+        self.pvExtendQ()
 
     def pvRetIndex(self,strState,reward):
         tr.call("clsAgent.pvRetIndex")
         for i in range(len(self.RewStates)):
             if strState == self.RewStates[i].state:
+                self.RewStates[i].visited +=1
                 return i
         # NewRewSate = typRewState(state,0,0,0)
         self.pvAppendNewState(typRewState(strState,reward,0,0))
-        return len(self.RewStates) - 1 #return index
+        i = len(self.RewStates) - 1
+        self.RewStates[i].visited +=1
+        return i #return index
 
     def pvExtendTransitionMatrix(self): #Type StateTransition: (1,3). From 1 to 3
         tr.call("clsAgent.pvExtendTransitionMatrix")
@@ -88,8 +101,20 @@ class clsAgent:
             if not (self.LastAction,ToIdx) in self.TransitionMatrix[FromIdx]:
                 self.TransitionMatrix[FromIdx].append((self.LastAction,ToIdx))
 
-
-
-
+    def pvExtendQ(self):
+        self.Q.append([])
+        for  _ in range(len(self.actions)):
+            self.Q[-1].append(0)
+    
+    def pvUpdateQ(self,alpha, gamma):
+        if len(self.SequenceRewards)<3: return
+        s1,r1,a = self.SequenceRewards[-1]
+        s,r,_ = self.SequenceRewards[-2]
+        r = r1-r
+        if "terminal" in str(self.RewStates[s].state):
+            self.SequenceRewards[-1] = (s1,0,a)
+            return
+        self.Q[s][a] = self.Q[s][a] + alpha*(r +gamma*max(self.Q[s1]) - self.Q[s][a])
+                       
 
 
