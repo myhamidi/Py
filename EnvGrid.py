89717b091bd499f621de826be50deaa36e5ff652
pbook <myhamidi@hotmail.de>
2019-06-29 08:39:59 +0200

    Update Env

--- EnvGrid.py
+++ EnvGrid.py
@@ -1,8 +1,5 @@
 import myTracer;tr = myTracer.glTracer
-import tkinter as tk
 import random
-import os
-import time
 
 class typGridState:
     def __init__(self,x,y,r,t):
@@ -16,14 +13,12 @@ class clsEnvironment:
     def __init__(self,rows,cols,reward):
         tr.call("clsEnvironment.init")
         self.EnvStates = [[typGridState(i,j,reward,False) for j in range(cols)] for i in range(rows)]
-        self.Grid = clsGrid(rows,cols)
         self.currentposition = (0,0)
         self.start = (0,0)
         self.limit_cols = cols
         self.limit_rows = rows
         self.step = 0
         self.run = 0
-        self.TKinterInit = False
         self.actions = ["up","down","left","right"]
         if rows == 1:
             self.actions = ["left","right"]
@@ -35,15 +30,15 @@ class clsEnvironment:
         i,j = pos
         return self.EnvStates[i][j]
 
-    def SetRestartPosition(self,StartPosition):
+    def SetStartPosition(self,StartPosition):
+        tr.call("clsEnv.SetStartPosition")
         r,c = StartPosition
-        if r >= 0 and r < self.limit_rows and c >= 0 and c < self.limit_cols:
-            self.start = StartPosition
-            self.currentposition = StartPosition
-        else:
-            print("error - SetStart")
+        assert r >= 0 and r < self.limit_rows and c >= 0 and c < self.limit_cols, "StartPosition out of Range"
+        self.start = StartPosition
+        self.currentposition = StartPosition
     
     def SetRandomStart(self):
+        tr.call("clsEnv.SetRandomStart")
         r = random.randint(0,self.limit_rows-1)
         c = random.randint(0, self.limit_cols-1)
         self.start = (r,c)
@@ -66,17 +61,32 @@ class clsEnvironment:
 
     def RetState(self):
         tr.call("clsEnv.RetState")
-        if self.IsStateTerminal() == True:
+        if self.IsCurrentStateTerminal() == True:
             self.run += 1
             return str(self.currentposition) + "terminal1"
         return str(self.currentposition)
 
-    def IsStateTerminal(self):
+    def RetGridAsArray(self):
+        tr.call("clsEnv.RetGridAsArray")
+        arr = []
+        for i in range(self.limit_rows):
+            arrRow = []
+            for j in range(self.limit_cols):
+                a = 0
+                if self.currentposition == (i,j): a = 1
+                arrRow.append(a)
+            arr.append(arrRow)
+        return arr
+
+    def IsCurrentStateTerminal(self):
         tr.call("clsEnv.IsStateTerminal")
         row, col = self.currentposition
         if self.EnvStates[row][col].terminal==True:
             return True
         return False
+
+    def Next(self,action):
+        self.move(action)
 
     def move(self,direction):
         tr.call("clsEnvironment.move")
@@ -95,89 +105,7 @@ class clsEnvironment:
         tr.call("clsEnv.returnActionList")
         return self.actions
 
-    def Next(self,action):
-        self.move(action)
-
     def Reset(self):
         self.step = 0
         self.run = self.run+1
         self.currentposition = self.start
-
-    def render(self,tim,typ):
-        if typ == "InConsole":
-            self.render_Console(tim)
-        if typ == "InTKinter":
-            self.MonitorUpdate(tim)
-
-    def render_Console(self,tim):
-        GridString = "+-------+"
-        emptyField = "| "
-        egoField = "|o"
-        tField = "|X"
-        os.system('cls')
-        for i in range(self.limit_rows):
-            GridString += "\n"
-            for j in range(self.limit_cols):
-                if (i,j) == self.currentposition:
-                    GridString += egoField
-                elif self.EnvStates[i][j].terminal==True:
-                    GridString += tField
-                else:
-                    GridString += emptyField
-            GridString += "|" 
-        GridString += "\n+-------+"
-        GridString += "\n"
-        GridString += "Position:" + str(self.currentposition)
-        print(GridString)
-        time.sleep(tim)
-        os.system('cls')
-    
-    def MonitorUpdate(self,tim):
-        tr.call("clsEnvironment.MonitorUpdate")
-        if self.TKinterInit == False:
-            self.Grid.show(int(tim*1000),0,0)
-            self.TKinterInit = True
-        terminal = False
-        if "terminal" in str(self.RetState()):terminal = True
-        self.Grid.update(self.currentposition,self.step,self.run,terminal)
-        self.Grid.show(int(tim*1000),0,0)
-
-class clsGrid:
-    def __init__(self,rows,cols):
-        tr.call("clsGrid.init")
-        self.limit_cols = cols
-        self.limit_rows = rows
-        self.EnvSteps = 0
-        self.EnvRuns = 0
-        self.TextHeight = 50
-        # <tkinter>
-        self.master = tk.Tk()
-        self.w = tk.Canvas(self.master, width=cols*50, height=self.TextHeight+rows*50)
-        self.w.pack()
-        self.element = [[self.w.create_rectangle(50*i, self.TextHeight+50*j, 50*i+50, self.TextHeight+50+50*j, fill="white") for i in range(cols)] for j in range(rows)]
-        self.txt_elements = [[self.w.create_text((25+50*i, self.TextHeight+25+50*j), text=str(i)+str(j)) for i in range(cols)] for j in range(rows)]
-        self.txt_header = self.w.create_text((cols*50/2, self.TextHeight/2), text="Run: " + str(self.EnvRuns) + ". Step: " + str(self.EnvSteps))
-        self.w.itemconfig(self.element[0][0],fill="#3395E4")
-        self.position = (0,0)
-
-    def show(self,tim,steps,run):
-        tr.call("clsGrid.show")
-        self.EnvSteps = steps
-        self.EnvRuns = run
-        if not tim == 99:
-            self.master.after(tim, self.w.quit)
-        tk.mainloop()
-
-    def res(self):
-        tr.call("clsGrid.res")
-        self.element = [[self.w.create_rectangle(50*i, self.TextHeight+50*j, 50*i+50, self.TextHeight+50+50*j, fill="white") for i in range(self.limit_cols)] for j in range(self.limit_rows)]
-        self.txt_elements = [[self.w.create_text((25+50*i, self.TextHeight+25+50*j), text=str(j)+str(i)) for i in range(self.limit_cols)] for j in range(self.limit_rows)]
-       
-
-    def update(self,currentposition,step,run,terminal):
-        tr.call("clsGrid.update")
-        x,y = currentposition
-        self.res()
-        self.w.itemconfig(self.element[x][y],fill="#3395E4")
-        if terminal == True: self.w.itemconfig(self.element[x][y],fill="#119111")
-        self.w.itemconfig(self.txt_header,text="Run: " + str(run) + ". Step: " + str(step))
--- testEnvGrid.py
+++ testEnvGrid.py
@@ -1,5 +1,6 @@
 import EnvGrid
 import Agt
+import Render
 
 ### Parameter:
 GZx = 10
@@ -8,17 +9,18 @@ GZy = 10
 ### Init
 Env = EnvGrid.clsEnvironment(GZx,GZy,-1)
 Agt = Agt.clsAgent(Env.ReturnActionList())
+Rnr = Render.clsGrid(GZx,GZy,"")
 
 ### Config
 Env.SetTerminalState((0,0)); Env.SetRewardAtState((0,0),0)
 Env.SetTerminalState((GZx-1,GZy-1)); Env.SetRewardAtState((GZx-1,GZy-1),0)
-Env.SetRestartPosition((int(GZx/2),int(GZy/2)))
+Env.SetStartPosition((int(GZx/2),int(GZy/2)))
 
 ### Train
 c = 0; runs_train = 3000
 while c < runs_train:
     Agt.PerceiveState(Env.RetState(),Env.RetReward())
-    if Env.IsStateTerminal():
+    if Env.IsCurrentStateTerminal():
         c+=1
         Env.Reset()
     if c%100 == 0: 
@@ -31,12 +33,13 @@ tmpState = ""; c = 0; runs_train = 10 #2*(GZ-1)
 Env.SetRandomStart()
 while c < runs_train:
     Agt.TakeState(Env.RetState(),Env.RetReward())
-    Env.render(0.1,"InTKinter")   #"InConsole"
+    # Env.render(0.01,"InTKinter")   #"InConsole"
     if Env.RetState() == tmpState:
         print("error - No State Change during Test " + tmpState + " " + str(c))
         break
     tmpState = Env.RetState()
-    if Env.IsStateTerminal():
+    Rnr.renderArray(Env.RetGridAsArray(),Env.RetState(),50)
+    if Env.IsCurrentStateTerminal():
         tmpState = ""; c+=1
         Env.Reset()
         Env.SetRandomStart()
