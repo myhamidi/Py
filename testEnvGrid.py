fe51278c3ec8a6311d7592141db0ff91d143619d
pbook <myhamidi@hotmail.de>
2019-07-05 11:31:14 +0200

    Update testEnvGrid.py

--- testEnvGrid.py
+++ testEnvGrid.py
@@ -23,10 +23,9 @@ while c < runs_train:
     if Env.IsCurrentStateTerminal():
         c+=1
         Env.Reset()
-    if c%100 == 0: 
-        print("Train Step: " + str(c),end ='\r')
     eps = 1-c/runs_train
     Env.Next(Agt.NextAction(eps))
+    if c%100 == 0:print("Train Step: " + str(c),end ='\r')
 
 ### Test
 tmpState = ""; c = 0; runs_train = 10 #2*(GZ-1)
@@ -35,9 +34,7 @@ while c < runs_train:
     Agt.TakeState(Env.RetState(),Env.RetReward())
     # Env.render(0.01,"InTKinter")   #"InConsole"
     Rnr.renderArray(Env.RetGridAsArray(),Env.RetState(),50)
-    if Env.RetState() == tmpState:
-        print("error - No State Change during Test " + tmpState + " " + str(c))
-        break
+    assert Env.RetState() == tmpState,"No State Change during Test. State is: " + tmpState 
     tmpState = Env.RetState()
     if Env.IsCurrentStateTerminal():
         tmpState = ""; c+=1
