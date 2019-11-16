6c3437417f9ed90f2acb5daeb363d43a96222ca0
myhamidi <myhamidi@hotmail.de>
2019-11-16 17:37:55 +0100

    Env Verification
    Added GroundTruth for Verification
    Removed Tracer

--- EnvGrid.py
+++ EnvGrid.py
@@ -1,17 +1,15 @@
-import myTracer;tr = myTracer.glTracer
 import random
 
 class typGridState:
     def __init__(self,x,y,r,t):
-        tr.call("typGridState.init")
         self.x=x
         self.y=y
         self.reward=r
         self.terminal=t
+        self.q = []             # used for verification only
     
 class clsEnvironment:
     def __init__(self,rows,cols,reward):
-        tr.call("clsEnvironment.init")
         self.EnvStates = [[typGridState(i,j,reward,False) for j in range(cols)] for i in range(rows)]
         self.currentposition = (0,0)
         self.start = (0,0)
@@ -27,19 +25,16 @@ class clsEnvironment:
             self.actions = ["up","down"]
 
     def __getitem__(self,pos):
-        tr.call("clsEnvironment.__getitem__")
         i,j = pos
         return self.EnvStates[i][j]
 
     def SetStartPosition(self,StartPosition):
-        tr.call("clsEnv.SetStartPosition")
         r,c = StartPosition
         assert r >= 0 and r < self.limit_rows and c >= 0 and c < self.limit_cols, "StartPosition out of Range"
         self.start = StartPosition
         self.currentposition = StartPosition
     
     def SetRandomStart(self):
-        tr.call("clsEnv.SetRandomStart")
         r = random.randint(0,self.limit_rows-1)
         c = random.randint(0, self.limit_cols-1)
         self.start = (r,c)
@@ -64,7 +59,6 @@ class clsEnvironment:
         return str(self.currentposition)
 
     def RetStateFeatures(self):
-        tr.call("clsEnv.RetStateFeatures")
         t = 0
         if self.IsCurrentStateTerminal() == True:
             self.run += 1
@@ -73,7 +67,6 @@ class clsEnvironment:
         return [x,y,t]
 
     def RetGridAsArray(self):
-        tr.call("clsEnv.RetGridAsArray")
         arr = []
         for i in range(self.limit_rows):
             arrRow = []
@@ -85,20 +78,21 @@ class clsEnvironment:
         return arr
 
     def IsCurrentStateTerminal(self):
-        tr.call("clsEnv.IsStateTerminal")
         row, col = self.currentposition
         if self.EnvStates[row][col].terminal==True:
             return True
         return False
 
     def Next(self,action):
+        if action == "":
+            self.move(random.choice(self.actions))
+            return
         if self.IsCurrentStateTerminal(): 
             self.Reset()
         else:
             self.move(action)
 
     def move(self,direction):
-        tr.call("clsEnvironment.move")
         self.step = self.step +1
         r,c = self.currentposition
         if direction == "left" and c >0:
@@ -111,7 +105,6 @@ class clsEnvironment:
             self.currentposition = (r+1,c)
 
     def ReturnActionList(self):
-        tr.call("clsEnv.returnActionList")
         return self.actions
     
     def ReturnFeatureList(self):
@@ -121,3 +114,40 @@ class clsEnvironment:
         self.step = 0
         self.run = self.run+1
         self.currentposition = self.start
+        
+
+# ==============================================================================
+# -- Verification --------------------------------------------------------------
+# ==============================================================================
+
+    def ReturnGroundTruthQTable(self):
+        tStates = []
+        for i in range(len(self.EnvStates)):
+            for j in range(len(self.EnvStates[i])):
+                if self.EnvStates[i][j].terminal == True:
+                    tStates.append([self.EnvStates[i][j].x,self.EnvStates[i][j].y])
+        assert len(tStates)==1 and str(tStates)== '[[0, 0]]',str(tStates)
+
+        self._setqValuesToEnvStates()
+        gtStates = []
+        for i in range(len(self.EnvStates)):
+            for j in range(len(self.EnvStates[i])):
+                t = 1 if self.EnvStates[i][j].terminal else 0
+                gtStates.append([[self.EnvStates[i][j].x, self.EnvStates[i][j].y, t] \
+                    , self.EnvStates[i][j].q])
+        return gtStates
+    
+    def _setqValuesToEnvStates(self):
+        for i in range(self.limit_rows):
+            for j in range(self.limit_cols):
+                reward = self.EnvStates[i][j].reward
+                q = float((i+j)*reward)
+                qup = q if i > 0 else q+reward
+                qdown = q+2*reward if i < self.limit_rows-1 else q+reward
+                qleft = q if j > 0 else q+reward
+                qright = q+2*reward if j < self.limit_cols-1 else q+reward
+                if i == 0 and j == 0:
+                    self.EnvStates[i][j].q = [0.0,0.0,0.0,0.0]
+                else:
+                    self.EnvStates[i][j].q = [qup,qdown,qleft,qright]
+        return
