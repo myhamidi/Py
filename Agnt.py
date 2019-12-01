95b310d564d103121fd20b4044eda1a971c4e36d
myhamidi <myhamidi@hotmail.de>
2019-12-01 23:43:24 +0100

    Upate Agnt

---
+++ Agnt.py
@@ -0,0 +1,116 @@
+# ==============================================================================
+# -- API -----------------------------------------------------------------------
+#
+# Agnt.clsAgent(["jump", "run"],["world","level","terminal"])   | Initiate Agent
+# Agt.SetParameter(["alpha","gamma"],[0.8,0.99])                | Set Agents parameter
+# Agt.PerceiveEnv([2,"street",0.1,0],-2)                        | Agent State and Reward Preception. Last index of list:
+#                                                               | 0 = non terminal; 1 = terminal state
+# Agt.Reset()                                                   | Delete Agt internal states and sequeces
+
+
+import random
+
+class typState:
+    def __init__(self, features = [] , reward = 0.0, Q = []):
+        self.features = features
+        self.reward = [reward, reward]
+        self.Q = Q
+        self.QUpdates = 0
+
+class typStep:
+    def __init__(self, state0 = [0], state1 = [0] , reward = 0, totalreward = 0.0, actionInt = -1, action = "", rg = "r"):
+        self.state0 = state0
+        self.state1 = state1
+        self.reward = reward
+        self.totalreward = totalreward
+        self.actionInt = actionInt
+        self.action = action
+        self.rg = rg
+        self.StepSampled = 0
+
+class clsAgent:
+#Public:
+    def __init__(self, actionlist, featurelist = []):
+        self.States = []         #Typ: typState. Remembers all (unique) states qUpdated.
+        self.Sequence = []
+        self.TerminalRewards = []       # idx, reward
+        self.actions = actionlist
+        self.actionsConstrains = actionlist
+        self.features = featurelist
+        self.Nterminal = 0
+
+        self.alpha = .1
+        self.gamma = 1.0
+        self.epsilon = 1.0
+        self.rand = list(range(1000))
+        random.shuffle(self.rand)
+        self.randIdx = 0
+        self.FitDQNStepSize = 10
+        self.DQNCounter = 0
+        self.ModelAlpha = 0.001
+
+        self.batchsize = 0
+        self.SequenceSampleLIST = []
+        self.SequenceSampleLISTXA = []
+        self.SequenceSampleLISTX = []
+        self.SequenceSampleTYP = []
+
+# ==============================================================================
+# -- Init ----------------------------------------------------------------------
+# ==============================================================================   
+    def SetParameter(self, parameter = [],value = []):
+        assert len(parameter)>0 and len(parameter)== len(value)
+        for i in range(len(parameter)):
+            if parameter[i] == "alpha":self.alpha = value[i]
+            if parameter[i] == "gamma":self.gamma = value[i]
+            if parameter[i] == "epsilon":
+                assert len(value[i]) == 1 or len(value[i]) == len(self.actions)
+                if len(value[i]) > 1:
+                    assert sum(value[i]) == 1
+                self.epsilon = value[i]
+
+
+# ==============================================================================
+# -- Perception ----------------------------------------------------------------
+# ============================================================================== 
+    def PerceiveEnv(self,state,reward):
+        self._UpdateSequence(state)
+        self._UpdateStatesTable(state)
+        self._RewardToStep(reward)
+        # self._UpdateNumTerminal()
+
+        
+    def _UpdateSequence(self,featState):
+        if len(self.Sequence)==0:
+            self.Sequence.append(typStep(state0 = featState))
+            return
+
+        if featState[-1] == 0:
+            self.Sequence.append(typStep(state0 = featState))
+            if self.Sequence[-2].state1 == [0]:self.Sequence[-2].state1 = featState 
+        else:
+            self.Sequence[-1].state1 = featState
+    
+    def _UpdateStatesTable(self,state):
+        for i in range(len(self.States)):
+             if state == self.States[i].features: 
+                return
+        self.States.append(typState(features = state, Q =[0]*len(self.actions)))
+
+    def _RewardToStep(self,reward):
+        self.Sequence[-1].reward = reward
+        self.Sequence[-1].totalreward = \
+        self.Sequence[-2].totalreward + reward if len(self.Sequence) > 1 else reward
+
+
+    def Reset(self):
+        self.States = []
+        self.Sequence = []
+
+# ==============================================================================
+# -- Return ---------------------------------------------------------------------
+# ==============================================================================     
+    def Info(self):
+        retStr = "Actions: " + str(self.actions) +"\n"
+        retStr += "State Features: " + str(self.features)
+        return retStr
---
+++ testAgnt.py
@@ -0,0 +1,82 @@
+import Agnt
+
+# -- Init ---------------------------------------------------------------------
+
+# Test1
+Agt = Agnt.clsAgent(["jump", "run"],["world","level","terminal"])
+retStr = "Actions: ['jump', 'run']"+"\n";retStr += "State Features: ['world', 'level', 'terminal']"
+assert Agt.Info() == retStr
+
+# Test2
+Agt.SetParameter(["alpha","gamma", "epsilon"],[0.8,0.99,[0.5]])
+assert Agt.alpha == 0.8
+assert Agt.gamma == 0.99
+assert Agt.epsilon == [0.5]
+
+# Test2
+Agt.SetParameter(["epsilon"],[[0.3, 0.7]])
+assert Agt.epsilon == [0.3, 0.7]
+
+# -- Perception ---------------------------------------------------------------------
+
+# Test3
+assert len(Agt.States) == 0
+assert len(Agt.Sequence) == 0
+Agt.PerceiveEnv([2,"hi",0.1,0],-2)
+assert len(Agt.States) == 1
+assert len(Agt.Sequence) == 1
+assert Agt.States[0].features == [2,"hi",0.1,0]
+assert Agt.Sequence[0].state0 == [2,"hi",0.1,0]
+
+# Test4
+Agt.Reset()
+assert len(Agt.States) == 0
+assert len(Agt.Sequence) == 0
+
+# Test5
+Agt.Reset()
+for i in range(100):
+    Agt.PerceiveEnv([i%10,100-i%10,0],-1)
+assert len(Agt.States) == 10
+assert len(Agt.Sequence) == 100
+
+# Test6
+Agt.PerceiveEnv([2,0],-1)
+Agt.PerceiveEnv([1,0],-1)
+assert Agt.Sequence[-2].state1 == [1,0]
+assert Agt.Sequence[-1].state0 == [1,0]
+
+# Test7
+Agt.Reset()
+Agt.PerceiveEnv([1,0],-2)
+Agt.PerceiveEnv([2,0],-3)
+assert Agt.Sequence[-2].reward == -2
+assert Agt.Sequence[-2].totalreward == -2
+assert Agt.Sequence[-1].reward == -3
+assert Agt.Sequence[-1].totalreward == -5
+
+#Test8
+Agt.Reset()
+for i in range(100):
+    Agt.PerceiveEnv([i%10,100-i%10,0],-1)
+assert Agt.Sequence[-1].totalreward == -100
+
+# -- Terminal ---------------------------------------------------------------------
+
+# Test: 
+Agt.PerceiveEnv(["A",0],-1)
+Agt.PerceiveEnv(["B",0],-1)
+Agt.PerceiveEnv(["C",1],-1)
+Agt.PerceiveEnv(["D",0],-1)
+Agt.PerceiveEnv(["E",0],-1)
+assert Agt.Sequence[-4].state0 == ["A",0]
+assert Agt.Sequence[-4].state1 == ["B",0]
+
+assert Agt.Sequence[-3].state0 == ["B",0]
+assert Agt.Sequence[-3].state1 == ["C",1]
+
+assert Agt.Sequence[-2].state0 == ["D",0]
+assert Agt.Sequence[-2].state1 == ["E",0]
+
+assert Agt.Sequence[-1].state0 == ["E",0]
+
